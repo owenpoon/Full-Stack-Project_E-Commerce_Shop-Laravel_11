9efc9d51af34252c13ece590063f1921a47525fc
Owen <poonhauwang6473@gmail.com>
2025-04-05 23:59:02 +0800

    seventh unfinished commit

--- eshop/app/Http/Controllers/AdminController.php
+++ eshop/app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use App\Models\Brand;
 use Carbon\Carbon;
 use Intervention\Image\Laravel\Facades\Image;
+use Illuminate\Support\Facades\File;
 
 class AdminController extends Controller
 {
@@ -27,7 +28,7 @@ class AdminController extends Controller
         $request->validate([ 
             'name' => 'required',
             'slug' => 'required|unique:brands,slug',
-            'image' => 'mimes:png,jpg,jpeg,svg|max:2048'
+            'image' => 'mimes:png,jpg,jpeg,svg|max:10000'
         ]);
 
         $brand = new Brand();
@@ -40,6 +41,36 @@ class AdminController extends Controller
         $brand->image = $file_name;
         $brand->save();
         return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully!');
+    }
+
+    public function brand_edit($id){
+        $brand = Brand::find($id);
+        return view('admin.brand-edit',compact('brand'));   
+    }
+
+    public function brand_update(Request $request){
+        $request->validate([ 
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug, '.$request->id,
+            'image' => 'mimes:png,jpg,jpeg,svg|max:10000'
+        ]);
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('uploads/brands').'/'.$brand->image)) {
+                File::delete(public_path('uploads/brands').'/'.$brand->image);
+            }
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention; 
+            $this->GenerateBrandThumbailsImage($image, $file_name);
+            $brand->image = $file_name;
+        }
+
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been updated successfully!');
     }
 
     public function GenerateBrandThumbailsImage($image, $imageName){
