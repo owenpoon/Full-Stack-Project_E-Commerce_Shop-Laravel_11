a2d4fa15defdd844b047eb216450e5054c422b12
Owen <poonhauwang6473@gmail.com>
2025-04-18 22:48:46 +0800

    ninth unfinished commit

--- eshop/app/Http/Controllers/AdminController.php
+++ eshop/app/Http/Controllers/AdminController.php
@@ -18,6 +18,7 @@ class AdminController extends Controller
         return view('admin.index');
     }
 
+    //Brand Functions
     public function brands(){
         $brands = Brand::orderBy('id', 'DESC')->paginate(10);
         return view('admin.brands', compact('brands'));
@@ -94,6 +95,7 @@ class AdminController extends Controller
         return redirect()->route('admin.brands')->with('status','Brand has been deleted successfully');
     }
 
+    //Category Functions
     public function categories(){
         $categories = Category::orderBy('id','DESC')->paginate(10);
         return view('admin.categories', compact('categories'));
@@ -130,5 +132,44 @@ class AdminController extends Controller
             $constraint->aspectRatio(); 
         })->save($destinationPath.'/'.$imageName);
     }
+
+    public function category_edit($id){
+        $category = Category::find($id);
+        return view('admin.category-edit', compact('category'));
+    }
+
+    public function category_update(Request $request){
+        $request->validate([ 
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug, '.$request->id,
+            'image' => 'mimes:png,jpg,jpeg,svg|max:10000'
+        ]);
+
+        $category = Category::find($request->id);
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('uploads/categories').'/'.$category->image)) {
+                File::delete(public_path('uploads/categories').'/'.$category->image);
+            }
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention; 
+            $this->GenerateCategoryThumbailsImage($image, $file_name);
+            $category->image = $file_name;
+        }
+
+        $category->save();
+        return redirect()->route('admin.categories')->with('status', 'Category has been updated successfully!');
+    }
     
+    public function category_delete ($id){
+        $category = Category::find($id);
+        if(File::exists(public_path('uploads/categories').'/'.$category->image)){
+            File::delete(public_path('uploads/categories').'/'.$category->image);
+        }
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status','Category has been deleted successfully');
+    }
+
 }
